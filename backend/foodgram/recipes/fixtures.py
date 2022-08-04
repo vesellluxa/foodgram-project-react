a2be401a0929b50fc9686ce13e13a9467ec93f3a
Алексей Шевич <lehashevich@gmail.com>
2022-08-05 00:01:46 +1100

    final

---
+++ backend/foodgram/recipes/fixtures.py
@@ -0,0 +1,18 @@
+import json
+
+from recipes.models import Product
+
+
+def fixture_maker():
+    data = json.load(
+        open('/Users/a1/PycharmProjects/finalgovna/'
+             'foodgram-project-react/data/ingredients.json',
+             "r", encoding="utf-8"))
+    for product in data:
+        Product.objects.create(
+            name=product['name'],
+            measurement_unit=product['measurement_unit']
+        )
+
+
+fixture_maker()
